df19335d559fd431e601daf0eeb56658f35fe00d
John Parnis <john.parnis.13@um.edu.mt>
2018-01-12 14:20:45 +0100

    setting up

---
+++ calc/cmake-build-debug/CMakeFiles/Beardos_Continuous_Integration_Assignment.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Beardos_Continuous_Integration_Assignment.dir/library.c.obj"
+  "libBeardos_Continuous_Integration_Assignment.pdb"
+  "libBeardos_Continuous_Integration_Assignment.dll"
+  "libBeardos_Continuous_Integration_Assignment.dll.a"
+  "libBeardos_Continuous_Integration_Assignment.dll.manifest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Beardos_Continuous_Integration_Assignment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ calc/cmake-build-debug/CMakeFiles/Beardos_Continuous_Integration_Assignment.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Beardos_Continuous_Integration_Assignment.
+# This may be replaced when dependencies are built.
